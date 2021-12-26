7ceff282ec791b380acc6ef941e13027c6fcc685
Aurelien Richeton <a.richeton@betclicgroup.com>
2021-12-26 17:41:21 +0100

    Remove dead code

--- plugins/template/src/main/kotlin/module/kotlin.gradle.kts
+++ plugins/template/src/main/kotlin/module/kotlin.gradle.kts
@@ -1,12 +1,5 @@
 package module
 
-import gradle.kotlin.dsl.accessors._fe4786bb74149843ef3aa76a3d93e592.implementation
-import gradle.kotlin.dsl.accessors._fe4786bb74149843ef3aa76a3d93e592.test
-import gradle.kotlin.dsl.accessors._fe4786bb74149843ef3aa76a3d93e592.testImplementation
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.kotlin
-import org.gradle.kotlin.dsl.repositories
-import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -21,10 +14,6 @@ repositories {
 dependencies {
     implementation(kotlin("bom"))
     testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
